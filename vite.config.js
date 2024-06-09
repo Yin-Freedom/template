0e5dd420cf6dcf113cdafb1fb2eee57ceda95a8b
Yin-Freedom <964663284@qq.com>
2024-06-09 14:32:49 +0800

    初始化

---
+++ vite.config.js
@@ -0,0 +1,30 @@
+import { defineConfig, loadEnv } from 'vite'
+import vue from '@vitejs/plugin-vue'
+import AutoImport from 'unplugin-auto-import/vite'
+import Components from 'unplugin-vue-components/vite'
+import { ElementPlusResolver } from 'unplugin-vue-components/resolvers'
+
+export default function ({ command, mode }) {
+  const root = process.cwd();
+  const env = loadEnv(mode, root);
+  return defineConfig({
+    base: './',
+    server: {
+      host: true,
+      port: 9095,
+      proxy: {
+        '/api': env.VITE_SERVER_ADDR
+      }
+    },
+    plugins: [
+      vue(),
+      AutoImport({
+        resolvers: [ElementPlusResolver()],
+      }),
+      Components({
+        resolvers: [ElementPlusResolver()],
+      }),
+    ]
+  })
+  
+}
